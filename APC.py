2c9be69eaf920a368eb09521297beb31fca9c61a
Matt <mateo13@gmail.com>
2015-06-05 13:33:41 -0400

    Added community string.

--- APC.py
+++ APC.py
@@ -17,18 +17,23 @@ class APCUnit:
 	reboot = 3 
 	rd = 7 # Reboot with Delay.
 	
-	def __init__(self, host, port=161 ):
+	def __init__(self, host, port=161, communityName='private' ):
 		""" Set up object with a host and port. """
 		self.host = host
 		self.port = port
-		self.community = cmdgen.CommunityData('my-agent', 'private', 0)
+		self.community = cmdgen.CommunityData('my-agent', communityName, 0)
 		self.target = cmdgen.UdpTransportTarget((host, port))
 	
 	def getOutletCtl(self, outlet):
 		""" Get the current state of an outlet. """
 		oid = self.outletCtlOID + (outlet,)
 		errorIndication, errorStatus, errorIndex, varBinds = cmdgen.CommandGenerator().getCmd(self.community, self.target, oid)
-		print(varBinds)
+		# add some error checking here maybe.
+		
+		return varBinds
+		#print('Error indication: ', errorIndication)
+		#print('Error status: ', errorStatus.prettyPrint())
+		#print('Error index: ', errorIndex)
 		
 	def setOutletCtl(self, outlet, state):
 		""" Set an outlet. """
@@ -45,5 +50,5 @@ if __name__ == "__main__":
 	""" We're being run as a script. """
 	a = APCUnit("10.52.190.226")
 	print(a.on)
-	a.getOutletCtl(1)
+	a.getOutletCtl(100)
 	a.setOutletCtl(1, a.reboot)
